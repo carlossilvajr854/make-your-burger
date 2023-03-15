ec649eef15b6fa13b9fc1555a923f36eba896114
Carlos Silva <carlossilvajr854@gmail.com>
2023-03-15 18:31:08 -0300

    implementacao-parte-home
    Implementação de algumas partes e funcionalidades da View Home.

--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,12 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import('../views/HomeView.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/pedidos',
+    name: 'PedidosView',
+    component: () => import(/* webpackChunkName: "pedidos" */ '../views/PedidosView.vue')
   }
 ]
 
